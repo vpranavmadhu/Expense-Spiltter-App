85091e3e1262698b4e40c497df629574658de4f8
Pranav Madhu <pranavmadhu2003@gmail.com>
2025-12-25 19:24:41 +0530

    Adds logger function to the postgres to see the queries

--- backend/internal/database/postgres.go
+++ backend/internal/database/postgres.go
@@ -3,18 +3,31 @@ package database
 import (
 	"esapp/configs"
 	"esapp/models"
+	"log"
+	"os"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func ConnectPostgres() (*gorm.DB, error) {
 
 	config := configs.LoadConfig()
 
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold: time.Second,
+			LogLevel:      logger.Info,
+			Colorful:      true,
+		},
+	)
+
 	dsn := config.DBUrl
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{Logger: newLogger})
 	if err != nil {
 		return nil, err
 	}
